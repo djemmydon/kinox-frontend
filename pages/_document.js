de0c3d7a7d483c6ba10a45bbda4115fa6f466172
unknown <okinopatrick0@gmail.com>
2023-04-19 15:16:16 +0100

    Added FB Meta pixel

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import { FB_PIXEL_ID } from '../lib/jspixel'
+import { Html, Head, Main, NextScript } from "next/document";
+import { FB_PIXEL_ID } from "../lib/jspixel";
 
 export default function Document() {
   return (
@@ -9,15 +9,20 @@ export default function Document() {
           <img
             height="1"
             width="1"
-            style={{ display: 'none' }}
+            style={{ display: "none" }}
             src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
           />
         </noscript>
+
+        <meta
+          name="facebook-domain-verification"
+          content="mgq7xhq6lvnr2a2yeuv9t3gc8i71q7"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
